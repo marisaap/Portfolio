1189315993afc26c6c0d9b3065de1bfcdee15358
marisaap <marisa.masini@outlook.com>
2026-05-11 19:50:56 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+const sobre-mim = document.queryselector("#Sobre-mim")
+const formação = document.queryselector("#formação")
+const portifólio = document.queryselector("#portfólio")
+const formulario = document.queryselector("#formulário")
+FormData.addEventListener('submit')
+
+
